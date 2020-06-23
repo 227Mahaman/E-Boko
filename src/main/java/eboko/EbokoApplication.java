850a6199938c2efc8d0d8a22495361cb2814d60e
227Mahaman <zalkiabass.456@gmail.com>
2020-06-23 21:01:31 +0100

    Dépôt initial

---
+++ src/main/java/eboko/EbokoApplication.java
@@ -0,0 +1,13 @@
+package eboko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbokoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eboko/EbokoApplicationTests.java
@@ -0,0 +1,13 @@
+package eboko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
